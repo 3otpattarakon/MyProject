7ea9074f6ed4ca9fb72d69a65ae305b4e141c073
3otpattarakon <pattarakon203@gmail.com>
2025-12-13 16:15:34 +0700

    Add files via upload

---
+++ Myproject/devrobot/src/main/java/com/myproject/robot/devrobot/DevrobotApplication.java
@@ -0,0 +1,13 @@
+package com.myproject.robot.devrobot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevrobotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevrobotApplication.class, args);
+	}
+
+}
