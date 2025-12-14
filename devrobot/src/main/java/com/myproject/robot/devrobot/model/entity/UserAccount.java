1feea4c629ba7bda6e5dea3fdfc4b62a2a613d90
3OT\pattt <pattarakon203@gmail.com>
2025-12-14 21:26:36 +0700

    Initialize Spring Boot project with MySQL integration and Maven wrapper

---
+++ devrobot/src/main/java/com/myproject/robot/devrobot/model/entity/UserAccount.java
@@ -0,0 +1,46 @@
+package com.myproject.robot.devrobot.model.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class UserAccount {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+
+    public UserAccount() {
+    }
+
+    public UserAccount(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
+
---
+++ devrobot/src/test/java/com/myproject/robot/devrobot/DevrobotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myproject.robot.devrobot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevrobotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
